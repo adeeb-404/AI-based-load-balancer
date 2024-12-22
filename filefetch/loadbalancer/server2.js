56c765ccacd37c203fc79bfefeecbc771102e55a
Adeeb <adeebm195@gmail.com>
2024-12-22 20:32:55 +0530

    change port number

--- filefetch/loadbalancer/server2.js
+++ filefetch/loadbalancer/server2.js
@@ -1,13 +1,12 @@
-import express from 'express';
-
+import express from "express";
 
 const app = express();
 app.use(express.json());
-app.post('/', (req, res) => {
-    console.log(req.body," request to server 2")
-  
-    res.send('Result from server 2');
+app.post("/", (req, res) => {
+  console.log(req.body, " request to server 2");
+
+  res.send("Result from server 2");
 });
-app.listen(5001, (req, res) => {
-    console.log('server 2 running on port 5001')
+app.listen(8452, (req, res) => {
+  console.log("server 2 running on port 5001");
 });
